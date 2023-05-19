a72ca4fd04d87658c2f9b6aa07f8e037789828db
David Umoru <umorudavido@gmail.com>
2023-05-19 11:09:00 +0100

    Update about import

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from "@/components/Navbar";
-import about from "./about";
+import About from "./about";
 
 const inter = Inter({ subsets: ['latin'] })
 
