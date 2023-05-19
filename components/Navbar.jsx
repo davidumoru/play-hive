8196f1b946dbbbdd5de6bcc88ab3a085c92530e5
David Umoru <umorudavido@gmail.com>
2023-05-19 03:51:01 +0100

    Modify links and styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import Wallet from "./Wallet";
 
 const Navbar = () => {
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
@@ -12,63 +11,48 @@ const Navbar = () => {
   };
 
   return (
-      <div className="navbar flex flex-col sm:flex-row justify-between items-center h-24 mx-auto px-4 sm:px-8 text-black">
-        <div className="flex items-center">
+      <div className="navbar flex flex-col align-middle items-center h-24 mx-auto px-40">
+        <div className="flex justify-between w-full h-full">
           <Link href="./Home">
-            <h1 className="text-3xl font-bold text-pink-500 mr-6">awujo.</h1>
+            <div className="text-3xl font-bold text-pink-500 mt-8">playHive.</div>
           </Link>
-          <ul className="flex items-center ml-4">
-            <li className="p-4 text-white font-normal transition-colors hover:text-pink-500 focus:text-pink-500">
-              <Link href="/Home">Home</Link>
+          <ul className="flex items-center">
+            <li className=" p-10 text-white font-normal transition-colors hover:text-pink-500 focus:text-pink-500">
+              <Link href="/home">Home</Link>
             </li>
-            <li className="p-4 text-white font-normal transition-colors hover:text-pink-500 focus:text-pink-500">
-              <Link href="/Games">Games</Link>
+            <li className=" p-10 text-white font-normal transition-colors hover:text-pink-500 focus:text-pink-500">
+              <Link href="/about">About</Link>
             </li>
-            <li className="p-4 text-white font-normal transition-colors hover:text-pink-500 focus:text-pink-500">
-              Leaderboard
+            <li className=" p-10 text-white font-normal transition-colors hover:text-pink-500 focus:text-pink-500">
+              <Link href="/games">Games</Link>
+            </li>
+            <li className="relative ml-4">
+              <button
+                className="flex items-center  p-10 text-white font-normal transition-colors hover:text-pink-500 focus:text-pink-500"
+                onClick={toggleDropdown}
+              >
+                Account
+                <svg
+                  className="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M16.59 8.59L12 13.17L7.41 8.59L6 10L12 16L18 10L16.59 8.59Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </button>
+              {isDropdownVisible && (
+                <ul className="absolute top-full right-0 mt-2 py-2 bg-white rounded-md shadow-xl">
+                  <li className="px-4 py-2 hover:bg-gray-100">Profile</li>
+                  <li className="px-4 py-2 hover:bg-gray-100">Logout</li>
+                </ul>
+              )}
             </li>
           </ul>
-          <li className="ml-4 p-2">
-            <input
-              type="text"
-              placeholder="Search"
-              className="border rounded-full px-4 py-2 text-black text-sm"
-            />
-          </li>
         </div>
-        <ul className="flex items-center ml-auto">
-          <li>
-            <Wallet />
-          </li>
-          <li className="ml-4 p-4 text-white font-normal transition-colors hover:text-pink-500 focus:text-pink-500">
-            Notifications
-          </li>
-          <li className="relative ml-4">
-            <button
-              className="flex items-center p-4 text-white font-normal transition-colors hover:text-pink-500 focus:text-pink-500"
-              onClick={toggleDropdown}
-            >
-              Account
-              <svg
-                className="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M16.59 8.59L12 13.17L7.41 8.59L6 10L12 16L18 10L16.59 8.59Z"
-                  fill="currentColor"
-                />
-              </svg>
-            </button>
-            {isDropdownVisible && (
-              <ul className="absolute top-full right-0 mt-2 py-2 bg-white rounded-md shadow-xl">
-                <li className="px-4 py-2 hover:bg-gray-100">Profile</li>
-                <li className="px-4 py-2 hover:bg-gray-100">Logout</li>
-              </ul>
-            )}
-          </li>
-        </ul>
       </div>
   );
 };
