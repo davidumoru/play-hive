e12b4edbcee820b092619cbe9a87826619af7fd3
David Umoru <umorudavido@gmail.com>
2023-05-19 03:59:19 +0100

    Modify styling

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-    <p className='flex justify-center mt-[20rem] text-[10rem] font-bold'>.Play Hive.</p>
-    <Link href="/about"><p className='flex justify-center text-[1.5rem]'>Read More...</p></Link>
+    <p className='flex  justify-center mt-[15rem] text-[10rem] font-bold'>Play Hive</p>
+    <Link href="/about"><p className='flex justify-center text-[1.5rem] hover:underline cursor-pointer hover:text-pink-400'>Read More...</p></Link>
     </>
   )
 }
