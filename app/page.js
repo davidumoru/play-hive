fd561a1d7100e86b14f3ea9375360b3013372c07
David Umoru <umorudavido@gmail.com>
2023-05-19 02:26:19 +0100

    Add title and "read more" link

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
+    <p className='flex justify-center mt-[20rem] text-[10rem] font-bold'>.Play Hive.</p>
+    <Link href="/about"><p className='flex justify-center text-[1.5rem]'>Read More...</p></Link>
     </>
   )
 }
